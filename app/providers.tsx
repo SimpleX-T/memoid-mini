254d7f757fa3cd0f1e8e17a2cf2e711a9b26aa24
SimpleX-T <ntmark2004@gmail.com>
2025-05-02 16:40:47 +0100

    commmitting 3

--- app/providers.tsx
+++ app/providers.tsx
@@ -1,10 +1,18 @@
 "use client";
 
-import { type ReactNode } from "react";
+import { useEffect, type ReactNode } from "react";
 import { base } from "wagmi/chains";
 import { MiniKitProvider } from "@coinbase/onchainkit/minikit";
+import sdk from "@farcaster/frame-sdk";
 
 export function Providers(props: { children: ReactNode }) {
+  useEffect(() => {
+    const init = async () => {
+      await sdk.actions.ready();
+    };
+    init();
+  }, []);
+
   return (
     <MiniKitProvider
       apiKey={process.env.NEXT_PUBLIC_ONCHAINKIT_API_KEY}
